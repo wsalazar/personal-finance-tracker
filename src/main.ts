fe0298320cb46703776dc27771d5252908da93de
Will Salazar <will.a.salazar@gmail.com>
2024-11-14 21:12:57 -0500

    removed console logs

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   });
-  console.log(process.env.PORT, 3000);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
